8ae017f708bb76cd51dec15fba323a0d91cefb1e
David Ehnert <17857925+DavidLokison@users.noreply.github.com>
2025-09-16 11:06:02 +0200

    init: add basic features for batch molding ingots

---
+++ BatchProductionMoldsModSystem.cs
@@ -0,0 +1,38 @@
+﻿using BatchProductionMolds.ModContent;
+using System;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace BatchProductionMolds
+{
+    public class BatchProductionMoldsModSystem : ModSystem
+    {
+        ICoreAPI? api;
+        ICoreClientAPI? capi;
+        ICoreServerAPI? sapi;
+
+        public override void Start(ICoreAPI api)
+        {
+            this.api = api;
+            
+            RegisterBlocks();
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            this.capi = api;
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            this.sapi = api;
+        }
+
+        private void RegisterBlocks()
+        {
+            if (api == null) throw new NullReferenceException();
+            api.RegisterBlockClass("BatchMolds.BlockBatchMold", typeof(BlockBatchMold));
+        }
+    }
+}
---
+++ Block/BlockBatchMold.cs
@@ -0,0 +1,23 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Util;
+using Vintagestory.GameContent;
+
+namespace BatchProductionMolds.ModContent
+{
+    public class BlockBatchMold : BlockToolMold
+    {
+        public override WorldInteraction[] GetPlacedBlockInteractionHelp(IWorldAccessor world, BlockSelection selection, IPlayer forPlayer)
+        {
+            WorldInteraction[] interactions = base.GetPlacedBlockInteractionHelp(world, selection, forPlayer);
+            foreach (WorldInteraction interaction in interactions)
+            {
+                if (interaction.ActionLangCode == "blockhelp-toolmold-takeworkitem")
+                {
+                    interaction.ActionLangCode = "batchmolds:blockhelp-batchmold-takeingots";
+                }
+            }
+            return interactions;
+        }
+    }
+}
